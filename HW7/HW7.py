eb3c22a28c4a316f6012920214b4353032d01744
Mykola Prystash <msprystash@gmail.com>
2022-09-15 17:04:15 +0300

    HW7_2

---
+++ HW7/HW7.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+arg_dict = {
+    "name": "useful",
+    "version": "1",
+    "description": "Very useful code",
+    "url": "http://github.com/dummy_user/useful",
+    "author": "Flying Circus",
+    "author_email": "flyingcircus@example.com",
+    "license": "MIT",
+    "packages": ["useful"],
+}
+
+requires = {'install_requires': 'requiers'}
+
+
+def do_setup(args_dict, requires):
+    setup(name=args_dict['name'],
+          version=args_dict['version'],
+          description=args_dict['description'],
+          url=args_dict['url'],
+          author=args_dict['author'],
+          author_email=args_dict['author_email'],
+          license=args_dict['license'],
+          packages=args_dict['packages'],
+          install_requires=requires['install_requires'])
