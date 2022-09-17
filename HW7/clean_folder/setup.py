7bfb8583bd0e02abfbc9f222990aed5d05c9e8c5
Mykola Prystash <msprystash@gmail.com>
2022-09-17 12:23:09 +0300

    HW7_final

--- HW7/clean_folder/setup.py
+++ HW7/clean_folder/setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
 
-
-# IrinaShushkevych
+setup(
+    name='clean_folder',
+    version='1.0.0',
+    description='Sort and clean folder script',
+    url='https://github.com/Sunriseuk/GoITHomeW/tree/main/HW7/clean_folder',
+    author='Mykola Prystash. Based on sorting script of IrinaShushkevych',
+    author_email='msprystash@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'clean-folder = clean_folder.clean:main']}
+)
